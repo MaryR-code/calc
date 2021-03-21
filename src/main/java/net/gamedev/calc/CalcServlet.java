9de99733912fb1b7da0b6d3c640a19bc96c7723f
MaryR-code <m.romanovskaia@gmail.com>
2021-03-21 18:04:21 +0200

    update 21-03-21

--- src/main/java/net/gamedev/calc/CalcServlet.java
+++ src/main/java/net/gamedev/calc/CalcServlet.java
@@ -1,5 +1,7 @@
 package net.gamedev.calc;
 
+import net.gamedev.calc.model.Player;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -8,18 +10,16 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @WebServlet(name = "CalcServlet", urlPatterns = "")
 public class CalcServlet extends HttpServlet {
-    private int number1;
-    private int number2;
-    private int sum;
+    private Player player = new Player();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         var rand = ThreadLocalRandom.current();
-        number1 = rand.nextInt(100, 1000);
-        number2 = rand.nextInt(100, 1000);
-        sum = number1 + number2;
-        request.setAttribute("num1", number1);
-        request.setAttribute("num2", number2);
+        player.setNum1(rand.nextInt(1, 10));
+        player.setNum2(rand.nextInt(1, 10));
+        request.setAttribute(Player.ATTR, player);
+        request.setAttribute("num1", player.getNum1());
+        request.setAttribute("num2", player.getNum2());
         request.getRequestDispatcher("/WEB-INF/calc.jsp")
                 .forward(request, response);
     }
@@ -27,19 +27,16 @@ public class CalcServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         var playerSum = Integer.parseInt(request.getParameter("playerSum"));
+        request.getAttribute(Player.ATTR);
+        var sum = player.getNum1() + player.getNum2();
         var msg = "";
         if (playerSum == sum) {
             msg = "Correct!";
         } else {
             msg = "Not correct!";
         }
-        openAnswer(request, response, msg);
-
-    }
-
-    private void openAnswer(HttpServletRequest request, HttpServletResponse response, String msg) throws ServletException, IOException {
-        request.setAttribute("num1", number1);
-        request.setAttribute("num2", number2);
+        request.setAttribute("num1", player.getNum1());
+        request.setAttribute("num2", player.getNum2());
         request.setAttribute("sum", sum);
         request.setAttribute("msg", msg);
         request.getRequestDispatcher("/WEB-INF/answer.jsp")
